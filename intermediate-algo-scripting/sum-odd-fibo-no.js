4159187b41dd7e2a2139558abf81b2bc07004d87
Sarang Wadode <sarangwadode@gmail.com>
2022-01-21 22:33:49 +0530

    sum of all odd fibo numbers upto given number(including)

---
+++ intermediate-algo-scripting/sum-odd-fibo-no.js
@@ -0,0 +1,24 @@
+// Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
+
+// The first two numbers in the Fibonacci sequence are 1 and 1. 
+// Every additional number in the sequence is the sum of the two previous numbers. 
+// The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
+
+// For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.
+
+function sumFibs(num) {
+    let fib = [0,1];
+    let i = 1;
+    while ((fib[i] + fib[i-1]) <= num ) {
+      i++;
+      fib[i] = fib[i-1] + fib[i-2]
+    }
+    let sum = 0;
+    for (let i=0; i < fib.length; i++) {
+      if( fib[i] % 2 === 1) {
+        sum += fib[i]
+      }
+    }
+    return sum
+  }
+  sumFibs(10);
