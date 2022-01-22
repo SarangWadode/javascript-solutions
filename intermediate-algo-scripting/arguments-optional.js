5cbd06220f3bd406c9fa9118eaabf7d2ed5a9b64
Sarang Wadode <sarangwadode@gmail.com>
2022-01-22 11:50:30 +0530

    Creating a function that sums two arguments together with some conditions

---
+++ intermediate-algo-scripting/arguments-optional.js
@@ -0,0 +1,17 @@
+// Create a function that sums two arguments together. 
+// If only one argument is pro?vided, then return a function that expects one argument and returns the sum.
+// For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.
+// Calling this returned function with a single argument will then return the sum:
+
+function addTogether() {
+    const [first, second] = arguments;
+    if (typeof(first) !== "number")
+      return undefined;
+    if (second === undefined)
+      return (second) => addTogether(first, second);
+    if (typeof(second) !== "number")
+      return undefined;
+    return first + second;
+  }
+  
+  addTogether(5)(7);
